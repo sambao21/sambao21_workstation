81de6adcae41db79f3d37de5f0740a75d12261cf
Sam Bao <sam.bao@cerner.com>
2013-03-24 16:35:50 -0500

    use my own iterm2 plist

---
+++ recipes/iterm2.rb
@@ -0,0 +1,6 @@
+include_recipe 'pivotal_workstation::iterm2'
+
+begin
+  t = resources(:cookbook_file => "#{WS_HOME}/Library/Preferences/com.googlecode.iterm2.plist")
+  t.cookbook "sambao21_workstation"
+end

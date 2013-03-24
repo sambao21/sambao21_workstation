7d9693f873572c38f725f8d3524083a9d1613a85
Sam Bao <sam.bao@cerner.com>
2013-03-24 13:04:53 -0500

    add google drive sha

--- recipes/google_drive.rb
+++ recipes/google_drive.rb
@@ -1,6 +1,6 @@
 dmg_package "Google Drive" do
   source "http://dl.google.com/drive/installgoogledrive.dmg"
-  #checksum ""
+  #checksum "7ab1add46f9617ec79ba291abd8f7bce4e22c172f660690b1d05457eb62a4445"
   owner WS_USER
   action :install
 end

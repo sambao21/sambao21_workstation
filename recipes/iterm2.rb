a57c2e9a6d18a76529408f44c9375d65a48eb7fc
Sam Bao <sambao21@gmail.com>
2013-03-24 17:27:35 -0500

    Update iterm2.rb
    check if installed first

--- recipes/iterm2.rb
+++ recipes/iterm2.rb
@@ -1,6 +1,8 @@
-include_recipe 'pivotal_workstation::iterm2'
+unless File.exists?("/Applications/iTerm.app")
+  include_recipe 'pivotal_workstation::iterm2'
 
-begin
-  t = resources(:cookbook_file => "#{WS_HOME}/Library/Preferences/com.googlecode.iterm2.plist")
-  t.cookbook "sambao21_workstation"
+  begin
+    t = resources(:cookbook_file => "#{WS_HOME}/Library/Preferences/com.googlecode.iterm2.plist")
+    t.cookbook "sambao21_workstation"
+  end
 end

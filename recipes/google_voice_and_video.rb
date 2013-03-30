a7a2c810dd81215c9a4a797aa183904a56d037f1
Sam Bao <sambao21@gmail.com>
2013-03-29 21:06:38 -0500

    update google voice to a pkg

--- recipes/google_voice_and_video.rb
+++ recipes/google_voice_and_video.rb
@@ -3,5 +3,7 @@ dmg_package "Google Voice And Video" do
   source "https://dl.google.com/googletalk/googletalkplugin/GoogleVoiceAndVideoSetup.dmg"
   checksum "428830e27e2b9e34dd570574f6ee03fd1194eb9d45e20b1945a1c3c72498092e"
   owner WS_USER
+  type "pkg"
+  package_id "com.google.pkg.GoogleVoiceAndVideo"
   action :install
 end

77a078b5d485981a141fd107e7c8b41b41677224
Sam Bao <sambao21@gmail.com>
2013-03-31 09:12:27 -0500

    use old hash syntax for active corners

--- recipes/active_corners.rb
+++ recipes/active_corners.rb
@@ -1,13 +1,13 @@
 active_corner_value_to_int = {
-  desktop:               4,
-  dashboard:             7,
-  mission_control:       2,
-  application_windows:   3,
-  launchpad:            11,
-  start_screen_saver:    5,
-  disable_screen_saver:  6,
-  put_display_to_sleep: 10,
-  noop:                  0
+  :desktop =>               4,
+  :dashboard =>             7,
+  :mission_control =>       2,
+  :application_windows =>   3,
+  :launchpad =>            11,
+  :start_screen_saver =>    5,
+  :disable_screen_saver =>  6,
+  :put_display_to_sleep => 10,
+  :noop =>                  0
 }
 
 node["active_corners"].each do |corner, value|

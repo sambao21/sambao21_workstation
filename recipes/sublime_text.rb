848234dc86adf10b48fbf4906bf3c016b9a30166
Sam Bao <sam.bao@cerner.com>
2013-04-27 17:25:15 -0500

    use rvm-auto-ruby for sublime text

--- recipes/sublime_text.rb
+++ recipes/sublime_text.rb
@@ -15,3 +15,9 @@ cookbook_file File.expand_path("Auto Encoding for Ruby.sublime-settings", File.j
   owner WS_USER
   mode "0600"
 end
+
+cookbook_file File.expand_path("Ruby.sublime-build", File.join(sublime_user_path)) do
+  source "Ruby.sublime-build"
+  owner WS_USER
+  mode "0600"
+end
